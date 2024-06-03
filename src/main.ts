ea4558450c4b1343ce721d5a7738900a56a47440
anthes1s <4nthesis@gmail.com>
2024-06-03 18:47:05 +0300

    update: implemented  command

--- src/main.ts
+++ src/main.ts
@@ -1,36 +1,44 @@
 import * as fs from 'fs/promises';
+import * as os from 'os';
+import * as ip from 'ip';
+import * as net from 'net';
 
 async function main() {
 	// TODO: Create handles for args: init, download, upload
 	// TODO: stash init [REPO NAME] -> create a directory and a .json file inside that directory that would define some properties of that stash (author, version, ip, port?, idfk)	
+	let srv: net.Server;
+
 	try {
 		const args: Array<string> = process.argv;
-		console.log(args);
-
 		for (let i = 0; i < args.length; i++) {
 			if (args[i] == 'init') {
-				console.log('init envoked');
 				const stashName: string = args[i + 1];
-				if (!stashName) throw new Error('missing repo name!');
+				if (!stashName) throw new Error('Missing stash name!');
 				fs.mkdir(`./${stashName}`);
-				console.log(`repo initiated`);
-				// TODO: Create a default stash.json file and write some shit into it
-				const defaultConfigFile = JSON.stringify({
-					author: "",
+
+				const defaultConfig = {
+					author: os.hostname(),
 					title: stashName,
 					version: "0.0.1",
-					ip: 'localhost', // TODO: create a class that would return localhost ip
+					ip: ip.address(),
 					port: 9999,
-					// NOTE: Maybe i'll need something else, but i don't know what at the moment
-				});
-				await fs.writeFile(`./${stashName}/stash.json`, defaultConfigFile);
+				};
+
+				await fs.writeFile(`./${stashName}/stash.json`, JSON.stringify(defaultConfig, null, 2));
+
+				// TODO: Now start a Server that would receive requests to download/upload a file
+				srv = net.createServer();
+				srv.on('connection', () => console.log(`somebody connected`));
+				srv.on('download', () => { console.log('somebody asked to download') });
+				srv.listen(defaultConfig.port, () => console.log(`stash is listening on port ${defaultConfig.port}`));
 			}
 		}
-
 		return 0;
 	} catch (error) {
-		console.log(error);
+		console.error(error);
 	}
 }
 
 main();
+
+// NOTE: I think I should implement handlers as EventEmmiter object. So, when I parse arguments I could emit events (e.g. .emit('init', options) .emit('upload', options) etc.) to handle them. 
